1cda3bc2d627793212780fd49b9d16b6ca325e75
Josue <73070103+joxpulp@users.noreply.github.com>
2020-12-18 23:44:29 -0500

    Add files via upload

---
+++ NUMEROS PARES E IMPARES.py
@@ -0,0 +1,16 @@
+﻿numerosImpares = 0
+numerosPares = 0
+
+numero = int (input ("Introduce un número o escriba 0 para detener:"))
+
+while numero != 0:
+    
+    if numero % 2 == 1:
+        numerosImpares += 1
+    else:
+        numerosPares += 1
+    numero = int (input ("Introduce un número o escriba 0 para detener:"))
+
+
+print("Números impares: ", numerosImpares)
+print("Números pares: ", numerosPares)
